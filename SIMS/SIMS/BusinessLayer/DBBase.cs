c4895ff0175c7c9ef33deeb97f941ebcc82922a0
is221312 <is221312@fhstp.ac.at>
2025-05-15 09:37:29 +0200

    update db string deleted
     ?? "Host=database-incident-app.camynshuuemw.ap-northeast-1.rds.amazonaws.com;Username=incidentappuser;Password=FwO0)]JKfmporr$J6ET67T-W5>#T;Database=postgres;Ssl Mode=Require;";

--- SIMS/SIMS/BusinessLayer/DBBase.cs
+++ SIMS/SIMS/BusinessLayer/DBBase.cs
@@ -6,6 +6,6 @@ namespace SIMS
     public class DBBase
     {
         public string ConnectionString { get; set; } =
-           Environment.GetEnvironmentVariable("postgresdb"); # ?? "Host=database-incident-app.camynshuuemw.ap-northeast-1.rds.amazonaws.com;Username=incidentappuser;Password=FwO0)]JKfmporr$J6ET67T-W5>#T;Database=postgres;Ssl Mode=Require;";
+           Environment.GetEnvironmentVariable("postgresdb");
     }
 }
